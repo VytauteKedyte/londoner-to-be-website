ded0341ebc74d8a85dc75fc6fd93b9fc2f302c06
Vytaute Kedyte <v.kedyte@gmail.com>
2018-03-18 19:37:10 +0000

    Added a results page draft, additional images and a few extra css elements

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,16 @@
 document.getElementById("enter").style.fontSize='100px';
+
+$('.carousel .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+
+  if (next.next().length>0) {
+    next.next().children(':first-child').clone().appendTo($(this));
+  }
+  else {
+    $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+  }
+});
